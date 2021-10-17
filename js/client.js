3ecc928bf5b12e503c5ba5925b98e40338d4120f
vibhore <vibhore.bhadanitech@gmail.com>
2021-10-17 20:09:36 +0530

    initial commit

---
+++ js/client.js
@@ -0,0 +1,44 @@
+const socket = io('http://localhost:8000');
+
+const form = document.getElementById('send-container');
+const messageInput = document.getElementById('messageInp');
+const messageContainer = document.querySelector(".container");
+var audio = new Audio('ting.mp3');
+
+const append = (message, position) => {
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageElement.classList.add('message')
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement);
+    if (position == 'left') {
+        audio.play();
+    }
+
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const message = messageInput.value;
+    append(`You: ${message}`, 'right');
+    socket.emit('send', message);
+    messageInput.value = '';
+})
+
+const nam = prompt("Enter your name to join");
+
+socket.emit('new-user-joined', nam);
+
+socket.on('user-joined', nam => {
+    append(`${nam} joined the chat`, 'right')
+})
+
+socket.on('receive', data => {
+    append(`${data.nam}: ${data.message}`, 'left')
+})
+
+socket.on('left', nam => {
+    append(`${nam} left the chat`, 'left')
+})
+
+
---
+++ nodeServer/index.js
@@ -0,0 +1,25 @@
+// node Server handle socket io connections
+const io = require('socket.io')(8000, {
+    cors: {
+        origin: '*',
+    }
+});
+
+const users = {};
+
+io.on('connection', socket => {
+    socket.on('new-user-joined', nam => {
+        // console.log("New user", nam)
+        users[socket.id] = nam;
+        socket.broadcast.emit('user-joined', nam);
+    })
+
+    socket.on('send', message => {
+        socket.broadcast.emit('receive', { message: message, nam: users[socket.id] })
+    });
+
+    socket.on('disconnect', message => {
+        socket.broadcast.emit('left', users[socket.id]);
+        delete users[socket.id];
+    });
+})
