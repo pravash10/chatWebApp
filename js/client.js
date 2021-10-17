1d4722eb8525600480254170901c2ead9fabbb50
pranav <pranav10vashistha@gmail.com>
2021-10-17 20:49:36 +0530

    efe

--- js/client.js
+++ js/client.js
@@ -1,5 +1,5 @@
 // const socket = io('http://localhost:8000');
-const socket = io('https://silly-montalcini-7eb8a9.netlify.app');
+const socket = io('https://silly-montalcini-7eb8a9.netlify.app:8000/socket.io/socket.io.js');
 
 const form = document.getElementById('send-container');
 const messageInput = document.getElementById('messageInp');
