8bb5146b17d6cfdb2cae035cb9a90a2e379adf2b
pranav <pranav10vashistha@gmail.com>
2021-10-17 20:44:54 +0530

    change url

--- js/client.js
+++ js/client.js
@@ -1,5 +1,5 @@
 // const socket = io('http://localhost:8000');
-const socket = io('https://silly-montalcini-7eb8a9.netlify.app/');
+const socket = io('https://silly-montalcini-7eb8a9.netlify.app');
 
 const form = document.getElementById('send-container');
 const messageInput = document.getElementById('messageInp');
